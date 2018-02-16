fa3b859d842a23715f6e2dcf08dc3fbde4e53b6b
Nityasri Sridhar <sridharkidambi@macbook-pro.attlocal.net>
2018-02-15 23:49:38 -0600

    first commit

---
+++ src/main/java/com/sridharkidambi/applications/springboot5weblearings/Springboot5weblearingsApplication.java
@@ -0,0 +1,12 @@
+package com.sridharkidambi.applications.springboot5weblearings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot5weblearingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot5weblearingsApplication.class, args);
+	}
+}
